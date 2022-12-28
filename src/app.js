8b785bd0a30f5129a9b34c0aede159bb9dd4de4d
jne101 <120770835+jne101@users.noreply.github.com>
2022-12-29 07:29:16 +0800

    Added relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -40,9 +40,18 @@ function displayTemperature(response) {
 
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
 let apiKey = "90fd2ea8306bo7aa2f0288345e56093t";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={London}&key=${apiKey}&units=metric`;
+let city = "London";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
