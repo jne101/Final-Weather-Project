6f5b2cca84a7b7556c4ee7cdc1101edd15f7f9d5
jne101 <120770835+jne101@users.noreply.github.com>
2022-12-30 06:18:47 +0700

    Added search engine in HTML & JS

--- src/app.js
+++ src/app.js
@@ -50,8 +50,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "90fd2ea8306bo7aa2f0288345e56093t";
-let city = "London";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "90fd2ea8306bo7aa2f0288345e56093t";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("London");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
